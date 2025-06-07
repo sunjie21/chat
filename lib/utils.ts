3152dbb43cfd2f88ab7b1bf67700083114c80866
sunjie21 <happysunjie21@gmail.com>
2025-06-07 14:09:34 +0800

    chore: init shadcn/ui

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,10 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 export function extractReactCode(content: string): string[] {
   const codes: string[] = [];
 
